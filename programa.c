6f5789dcdaa8757ba1c961f78b310c77d315d6e0
sulem <sulem477@gmail.com>
2024-10-23 11:20:11 -0600

    CWE-401

---
+++ programa.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define LOOPS 100
+#define MAXSIZE 256
+
+int main(int argc, char **argv)
+{
+    int count = 0;
+    char *pointer = NULL;
+
+    for(count = 0; count < LOOPS; count++)
+    {
+        pointer = (char *) malloc(sizeof(char) * MAXSIZE);
+        free(pointer);
+    }
+    return count;
+}
